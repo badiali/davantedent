c18dff052dde79e2282399271cd3e918e559656f
Luis Badiali <luis@badiali.es>
2026-01-02 16:07:06 +0100

    Funciones para el LocalStorage

--- js/app.js
+++ js/app.js
@@ -22,3 +22,16 @@ class Cita {
     this.observaciones = observaciones;
   }
 }
+
+// --- ALMACENAMIENTO (LOCALSTORAGE) ---
+
+// Recuperar el array de citas o un array vacío si es la primera
+function obtenerCitas() {
+  const citas = localStorage.getItem("citasDavanteDent");
+  return citas ? JSON.parse(citas) : [];
+}
+
+// Guardar el array actualizado
+function guardarCitasEnStorage(citas) {
+  localStorage.setItem("citasDavanteDent", JSON.stringify(citas));
+}
