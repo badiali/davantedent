820ec2c400e535ddf4a3a7365176d23efbef4e72
Luis Badiali <luis@badiali.es>
2026-01-02 16:05:42 +0100

    Implementación de la clase Cita

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// --- CLASE CITA ---
+class Cita {
+  constructor(
+    id,
+    fecha,
+    hora,
+    nombre,
+    apellidos,
+    dni,
+    telefono,
+    fechaNacimiento,
+    observaciones
+  ) {
+    this.id = id;
+    this.fecha = fecha;
+    this.hora = hora;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.dni = dni;
+    this.telefono = telefono;
+    this.fechaNacimiento = fechaNacimiento;
+    this.observaciones = observaciones;
+  }
+}
